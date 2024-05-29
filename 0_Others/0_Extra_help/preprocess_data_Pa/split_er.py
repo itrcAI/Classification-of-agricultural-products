d5a9832a2993b4325d7c6d332bdbfc35944907b1
shakouri <erfanshakouri.aielec@gmail.com>
2024-05-29 06:09:50 +0330

    totaly

---
+++ 0_Others/0_Extra_help/preprocess_data_Pa/split_er.py
@@ -0,0 +1,52 @@
+# %%
+import random
+
+from util import get_all_npy
+from util import copy_paste
+from util import create_folder
+
+# %%
+def split_test_train(path_file_npy : str,
+                     path_to_save : str,
+                     test_size : int = 0.2 ) -> None:
+    
+    npy_files = list(get_all_npy(path_file_npy).values())
+    number_test = int(
+            len(npy_files) * test_size)
+    
+    
+    test_data = random.sample(npy_files, number_test)
+    
+    
+        
+    folder_split = path_to_save + "/DATA_split"
+    create_folder(folder_split)
+    
+            
+    folder_train = folder_split + "/train"
+    create_folder(folder_train)
+    
+    for train_data in npy_files:
+        if train_data not in test_data:
+            copy_paste(train_data, folder_train)
+
+
+    folder_test = folder_split + "/test"
+    create_folder(folder_test)
+    
+    for _test_data in test_data:
+        copy_paste(_test_data, folder_test)
+    
+    
+    print("test / train saved in path ...")
+
+
+
+# %%
+split_test_train("/home/mhbokaei/shakouri/SatelliteImage_TimeSeries_Classification/dataset_folder/DATA", "/home/mhbokaei/shakouri/test/Satellite/dataset_folder_train", 0.2)
+
+
+# %%
+
+
+
